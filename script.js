23fdfe66fd3a19e5cdf71c32ee770ca86645aa35
student <add.pint@gmail.com>
2021-07-07 23:41:30 +0200

    app version 1.0

---
+++ script.js
@@ -0,0 +1,32 @@
+function selectCard(card) {
+    // code goes here to select a card  
+}
+
+function addWork() {
+    // code goes here for add work mode
+    let node = document.createElement("li");
+    let textnode = document.createTextNode("");
+    node.appendChild(textnode);
+}
+
+function deleteWork() {
+    // code goes here to delete a work in a card
+  
+}
+
+function update() {
+    // code goes here to update card title
+}
+
+function clearWorkList() {
+    //code goes here to clear workList
+  
+}
+
+function changeMode() {
+  // code goes here switch between add work, update card title and delete work
+}
+
+function deleteMode() {
+  // code goes here for delete Mode
+}
